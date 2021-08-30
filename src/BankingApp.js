d736d96b11dd959aa8b44f570e868d867f9fac75
elowen <jackiesalasdev@gmail.com>
2021-08-30 11:02:56 +0000

    used match and url for nested routes

--- src/BankingApp.js
+++ src/BankingApp.js
@@ -1,4 +1,4 @@
-import { NavLink, Switch, Route } from "react-router-dom";
+import { NavLink, Switch, Route, useRouteMatch } from "react-router-dom";
 import Deposit from "./comps/Admin/Deposit/Deposit";
 import Withdraw from "./comps/Admin/Withdraw/Withdraw";
 import Transfer from "./comps/Admin/Transfer/Transfer";
@@ -8,8 +8,10 @@ import { useSetAuth, useGetAuth } from "./context/Auth/AuthProvider";
 import { Redirect } from "react-router-dom";
 
 export default function BankingApp() {
+  const {path,url} = useRouteMatch()
   const setAuth = useSetAuth();
   const user = useGetAuth();
+  
 
   function handleSignout() {
     setAuth({ type: "logout" });
@@ -27,10 +29,10 @@ export default function BankingApp() {
         SIGN OUT
       </button>
       <nav className="admin__nav">
-        <NavLink to="/admin/deposit">Deposit</NavLink>
-        <NavLink to="/admin/withdraw">Withdraw</NavLink>
-        <NavLink to="/admin/transfer">Transfer</NavLink>
-        <NavLink to="/admin/create">Create Account</NavLink>
+        <NavLink to={`${url}/deposit`}>Deposit</NavLink>
+        <NavLink to={`${url}/withdraw`}>Withdraw</NavLink>
+        <NavLink to={`${url}/transfer`}>Transfer</NavLink>
+        <NavLink to={`${url}/create`}>Create Account</NavLink>
       </nav>
 
       <div className="admin__accounts">
@@ -38,10 +40,10 @@ export default function BankingApp() {
       </div>
       <div className="admin__forms">
         <Switch>
-          <Route path="/admin/deposit" component={Deposit} />
-          <Route path="/admin/withdraw" component={Withdraw} />
-          <Route path="/admin/transfer" component={Transfer} />
-          <Route path="/admin/create" component={Creator} />
+          <Route path={`${url}/deposit`} component={Deposit} />
+          <Route path={`${url}/withdraw`} component={Withdraw} />
+          <Route path={`${url}/transfer`} component={Transfer} />
+          <Route path={`${url}/create`} component={Creator} />
         </Switch>
       </div>
     </div>
